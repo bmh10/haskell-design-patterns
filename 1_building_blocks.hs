033d66386f399c4825777c10756afa61b35575b6
bmh10 <bmhomer13@gmail.com>
2016-12-20 20:26:14 +0000

    Tail vs non-tail recursion

--- 1_building_blocks.hs
+++ 1_building_blocks.hs
@@ -26,3 +26,22 @@ res = uncurry max (g 11)
 -- Decoupling with currying
 -- Partial function application can be used for decoupling
 
+-- Recursion
+-- Functions and types can be recursive
+
+-- Not-tail recursion
+sumNonTail [] = 0
+sumNonTail (x:xs) = x + (sumNonTail xs)
+-- This is not tail-recursive because the recursion is 'trapped' by the + operator.
+-- This requires whole list to be stored in memory for the sum to be performed.
+
+-- Tail recursion
+sumTail' acc [] = acc
+sumTail' acc (x:xs) = sumTail' (acc + x) xs
+sumTail xs = sumTail' 0 xs
+-- This uses constant space and is therefor more efficient.
+
+--sumNonTail is a recursive function that expresses a recursive process.
+--sumTail is a recursive function that expresses an iterative process.
+
+
