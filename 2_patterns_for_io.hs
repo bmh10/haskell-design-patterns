3ad562ac11fa52ca3087b25d38cc86e89025cc4f
bmh10 <bmhomer13@gmail.com>
2017-01-02 16:52:25 +0000

    I/O as functor, applicative and monad

--- 2_patterns_for_io.hs
+++ 2_patterns_for_io.hs
@@ -26,4 +26,23 @@ main = do
 
 -- I/O as a functor, applicative, and monad
 
+-- I/O monad is also an applicative functor, which in turn is a functor.
+-- I/O as a functor allows us to use the fmap function e.g:
+  line <- fmap (show . words) (hGetLine h)
+  putStrLn line
+
+-- I/O as an applicative functor means we can use this syntax instead of fmap:
+  line <- (show . words) <$> (hGetLine h)
+  putStrLn line
+
+-- Monadic version:
+  line <- liftM (show . words) (hGetLine h)
+  putStrLn line
+
+-- These 3 styles are all equivalent in this case.
+-- Monad is more power than applicative, applicative is more powerful than functor.
+-- Monad enables us to compose I/O actions together into seqeunced pipelines.
+-- Applicative and functor allow us to apply functions to I/O actions.
+
+-- Imperative I/O
 
