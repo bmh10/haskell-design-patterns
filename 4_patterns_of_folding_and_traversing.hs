3f7b18641079c4b911146502079cf95ec7b3c5dd
bmh10 <bmhomer13@gmail.com>
2017-02-11 09:19:07 +0000

    Folding over lists

--- 4_patterns_of_folding_and_traversing.hs
+++ 4_patterns_of_folding_and_traversing.hs
@@ -1,3 +1,31 @@
 -- Patterns of Folding and Traversing
 
+-- With the introduction of Applicative came more powerful mapping (traversal), which enabled type-level folding and mapping.
+-- First we look at how the Prelude's list fold is generalized to all Foldable containers.
+-- Then we follow the generalization of the list map to all Traversable containers.
+-- We then look at the Lens library which raises Foldable and Traversable to an even higher level of abstraction.
 
+-- Folding over lists
+
+-- Recall from Chapter 1:
+
+sumLazy []     = 0
+sumLazy (x:xs) = x + sumLazy xs
+
+-- This is captured by the recursive process foldr:
+
+sumLazy' = foldr (+) 0
+
+-- We can also write a strict sum function:
+sumStrict acc []     = acc
+sumStrict acc (x:xs) = sumStrict (acc + x) xs
+
+-- This is captured by foldl:
+
+sumStrict' = foldl (+)
+
+-- The foldl function is tail recursive and strict in the accumulator.
+-- Foldr is non-tail recursive and lazy.
+-- Foldl is a special case of foldr.
+
+-- Folding with monadic functions
