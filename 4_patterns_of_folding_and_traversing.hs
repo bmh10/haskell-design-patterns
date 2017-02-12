e4a2cc43cf87b362c5f425bf6cb80a639497a1bf
bmh10 <bmhomer13@gmail.com>
2017-02-12 11:59:42 +0000

    Folding with monadic functions

--- 4_patterns_of_folding_and_traversing.hs
+++ 4_patterns_of_folding_and_traversing.hs
@@ -29,3 +29,31 @@ sumStrict' = foldl (+)
 -- Foldl is a special case of foldr.
 
 -- Folding with monadic functions
+
+doSumStrict :: (Show a, Num a) => a -> [a] -> IO a
+
+doSumStrict acc [] = return acc
+doSumStrict acc (x:xs) = do
+  putStrLn $ " + " ++ (show x) ++ " = " ++ (show acc')
+  doSumStrict acc' xs
+  where acc' = acc + x
+
+main = doSumStrict 0 [2, 3, 5, 7]
+
+-- To write as a left-fold, we use the foldM function:
+doSumStrict' = foldM doPlus
+  where doPlus acc x = do
+    putStrLn $ " + " ++ (show x) ++ " = " ++ (show acc)
+    return (acc + x)
+
+main = doSumStrict' 0 [2, 3, 5, 7]
+
+-- Both foldl/r and foldM describe folding over lists:
+foldM :: Monad m => (b -> a -> m b) -> b -> [a] -> m b
+foldl ::            (b -> a -> b)   -> b -> [a] -> b
+
+-- For foldM, the fold function is monadic and the result accumulates inside the Monad class.
+
+-- Folding with Monoids
+
+
