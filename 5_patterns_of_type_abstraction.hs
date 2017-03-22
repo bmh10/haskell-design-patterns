447d7b4dc879a48444750311f03a7fc908749c23
bmh10 <bmhomer13@gmail.com>
2017-03-22 10:32:28 +0000

    tupleF

--- 5_patterns_of_type_abstraction.hs
+++ 5_patterns_of_type_abstraction.hs
@@ -11,4 +11,24 @@
 
 -- Abstracting function types: RankNTypes
 
+-- Consider the higher order function that maps the argument function to each tuple element:
 
+tupleF elemF (x, y) (elemF x, elemF y)
+
+-- Left to its own devices, the Haskell 98 compiler will infer this type for a tupleF function:
+
+tupleF :: (a -> b) -> (a, a) -> (b, b)
+
+-- As elemF is applied to x and y, the compiler assumes that x and y must be of the same type, hence the inferred tuple type (a, a).
+-- This allows us to do the following:
+
+tupleF length ([1, 2, 3], [3, 2, 1])
+tupleF show (1, 2)
+tupleF show (True, False)
+
+-- But not this:
+
+tupleF show (True, 2)
+tupleF length ([True, False, False], [1, 2, 4])
+
+  
