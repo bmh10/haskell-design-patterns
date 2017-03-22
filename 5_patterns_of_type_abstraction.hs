6b815a2b7daa50b6a12ef20da8fe44a76b246064
bmh10 <bmhomer13@gmail.com>
2017-03-22 10:28:10 +0000

    C5 intro

---
+++ 5_patterns_of_type_abstraction.hs
@@ -0,0 +1,14 @@
+-- Patterns Of Type Abstraction
+
+-- In this chapter we look at some of the key advances on one of the major fronts of Haskell's evolution: language extensions.
+
+-- Haskell extensions are tied to the compiler implementation rather than the language standard.
+-- We'll explore the extensions along three axes: abstracting functions, datatypes, and type-classes.
+
+-- 1. Abstracting function types: RankNTypes
+-- 2. Abstracting datatypes: Existential quantification, phantom types, generalized algebraic datatypes (GADTs), type case pattern, dynamic types, and heterogeneous lists.
+-- 3. Abstracing type-classes: Multiparameter type-classes and functional dependencies.
+
+-- Abstracting function types: RankNTypes
+
+
