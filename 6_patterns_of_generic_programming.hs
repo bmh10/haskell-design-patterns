f6b47bd69efa669e434b88405d785689138d5d31
bmh10 <bmhomer13@gmail.com>
2017-04-01 09:16:26 +0100

    C6 intro

---
+++ 6_patterns_of_generic_programming.hs
@@ -0,0 +1,15 @@
+-- 6. Patterns of Generic Programming
+
+-- In this chapter we seek a unified view of Generic Programming.
+-- We start with a broad perspective by looking at Jeremy Gibbons' patterns of generic programming.
+-- We then shift focus to a pattern: datatype-generic programming - which is characterized by generic functions parameterized by the shape of the datatype instead of the content.
+
+-- We sample three basic approaches to datatype-generic programming: sum of products, origami programming, and scrapping your boilerplate.
+
+-- We will also encounter a few exotic Haskell types - Typeable and Data; Bifunctor and Fix.
+-- We will reveal the underpinning of Derivable type classes.
+-- We will also discover the Gang of Four design patterns at the heart of datatype-generic programming.
+
+
+-- Patterns of Generic Programming
+
